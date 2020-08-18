cacb310502b27dcc123e7fce3eeaf37a15fc1207
lenovo <kdh0161@naver.com>
2020-08-18 09:26:49 +0900

    db 간단한 연동 추가

--- src/unit01/AdditonServlet01.java
+++ src/unit01/AdditonServlet01.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.sun.xml.internal.bind.v2.TODO;
 
 import unit01.dao.TitleDao;
 import unit01.dao.impl.TitleDaoImpl;
@@ -20,51 +23,64 @@ import unit01.dto.Title;
 @WebServlet("/AdditonServlet01")
 public class AdditonServlet01 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AdditonServlet01() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/*
+	 * TitleDao tt;
+	 * 
+	 * @Override public void init() throws ServletException { // TODO Auto-generated
+	 * method stub super.init(); }
+	 * 
+	 * @Override public void init(ServletConfig config) throws ServletException { //
+	 * TODO Auto-generated method stub super.init(config); }
+	 */
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public AdditonServlet01() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		TitleDao dao = new TitleDaoImpl();
-		
-		  int no = Integer.parseInt(request.getParameter("no"));
-		  String name = request.getParameter("name"); 
-		  Title newTitle = new Title(no, name);
-		  
-		  dao.insertTitle(newTitle);
-		  
-		    List<Title> list = dao.selectTitleByAll();
-		    
-		  for(Title t : list) {
-			  System.out.println(t);
-		  }
-		  
-		 
+
+		int no = Integer.parseInt(request.getParameter("no"));
+		String name = request.getParameter("name");
+		Title newTitle = new Title(no, name);
+
+		dao.insertTitle(newTitle);
+
+		List<Title> list = dao.selectTitleByAll();
+
+		for (Title t : list) {
+			System.out.println(t);
+		}
+
 		int num1 = Integer.parseInt(request.getParameter("num1"));
 		int num2 = Integer.parseInt(request.getParameter("num2"));
 		int add = num1 + num2;
-		
+
 		request.setAttribute("num1", num1);
 		request.setAttribute("num2", num2);
 		request.setAttribute("add", add);
 		request.setAttribute("list", list);
-		
+
 		RequestDispatcher dispatcher = request.getRequestDispatcher("add.jsp");
 		dispatcher.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
